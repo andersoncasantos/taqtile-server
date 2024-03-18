8b2ed5f2d9e0a05e9da9b47554cdb9be403388bc
andersoncasantos <andersonmtv07@gmail.com>
2024-03-17 21:31:01 -0300

    new branch eslint-prettier

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const { createHandler } = require("graphql-http/lib/use/express")
-const { buildSchema } = require("graphql")
-const { ruruHTML } = require("ruru/server")
+const express = require('express');
+const { createHandler } = require('graphql-http/lib/use/express');
+const { buildSchema } = require('graphql');
+const { ruruHTML } = require('ruru/server');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
+`);
 
 // The root provides a resolver function for each API endpoint
 const root = {
     hello: () => {
         return "Hello world!"
     },
-}
+};
 
 const app = express()
 
@@ -26,14 +26,14 @@ app.all(
         schema: schema,
         rootValue: root,
     })
-)
+);
 
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
     res.type("html")
     res.end(ruruHTML({ endpoint: "/graphql" }))
-})
+});
 
 // Start the server at port
-app.listen(4000)
-console.log("Running a GraphQL API server at http://localhost:4000/graphql")
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
